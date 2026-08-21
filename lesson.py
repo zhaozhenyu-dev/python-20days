60a22ce6b0572ee92919b0cc2662c29ff389fcbf
zhaozhenyu-dev <zhaozhenyu@example.com>
2026-08-22 06:36:10 +0800

    day,完成猜字游戏

--- lesson.py
+++ lesson.py
@@ -1,4 +1,15 @@
-print("你好，振宇")
-name="赵振宇"
-age=21
-print(f"{name}{age}岁")
+import random                        # 借用随机数工具箱
+
+secret = random.randint(1, 100)      # 生成答案，randint(1,100)=抽1~100的随机整数
+count = 0                            # 计数器：猜了几次
+
+while True:                          # 开始无限循环
+    guess = int(input("猜一个 1~100 的数："))   # 每轮让玩家猜一次
+    count = count + 1                # 每猜一次，count 加 1
+    if  guess>secret:
+        print("大了，再试试")
+    elif guess<secret:
+        print("小了，再试试")
+    else:
+        print(f"恭喜！你猜了 {count} 次")
+        break
